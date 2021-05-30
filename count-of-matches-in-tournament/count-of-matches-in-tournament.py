ea75b5446dee189428ed246014c729d569192dc2
earth <70900028+earth-h@users.noreply.github.com>
2021-05-30 12:33:00 +0900

    Time: 36 ms (50.43%), Space: 14.3 MB (34.92%) - LeetHub

--- count-of-matches-in-tournament/count-of-matches-in-tournament.py
+++ count-of-matches-in-tournament/count-of-matches-in-tournament.py
@@ -1,11 +1,7 @@
 class Solution:
     def numberOfMatches(self, n: int) -> int:
         ans = 0
-        while n != 1:
-            if n % 2 == 0: # even
-                ans += n // 2
-                n //= 2
-            else:
-                ans += (n - 1) // 2
-                n = (n - 1) // 2 + 1
+        while n > 1:
+            ans += n // 2 
+            n = n // 2 + (n & 1)
         return ans
