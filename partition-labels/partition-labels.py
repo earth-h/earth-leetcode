6f66b2587cc9692ec6de043f116d35b5651e45d3
earth <70900028+earth-h@users.noreply.github.com>
2021-06-06 10:37:57 +0900

    Time: 36 ms (86.63%), Space: 14.2 MB (55.72%) - LeetHub

--- partition-labels/partition-labels.py
+++ partition-labels/partition-labels.py
@@ -1,5 +1,19 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
+        sList = {k: v for v, k in enumerate(s)}
+        # ex) {'a': 8, 'b': 5, 'c': 7, 'd': 14, 'e': 15, 'f': 11, 'g': 13, 'h': 19, 'i': 22, 'j': 23, 'k': 20, 'l': 21}
+        ans = list()
+        
+        tmpLocation = 0
+        while tmpLocation < len(s):
+            maxLocation = sList[s[tmpLocation]]
+            curLocation = tmpLocation
+            while tmpLocation <= maxLocation:
+                if sList[s[tmpLocation]] > maxLocation:
+                    maxLocation = sList[s[tmpLocation]]
+                tmpLocation += 1
+            ans.append(maxLocation - curLocation + 1)
+        '''
         #print(ord('z') - ord('a') + 1) # the number of alphabet
         #alphabet = [0 for _ in range(ord('z') - ord('a') + 1)]
         alphabet = [[0,0] for _ in range(26)]
@@ -27,5 +41,5 @@ class Solution:
             curLocation = maxNum + 1
             ans.append(curLocation - lastLocation)
             lastLocation = curLocation
-            
+        '''
         return ans
