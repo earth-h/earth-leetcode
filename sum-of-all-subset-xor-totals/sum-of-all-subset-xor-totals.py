1416e5e1293a34217a813c1a40417383b8b2e9a2
earth <70900028+earth-h@users.noreply.github.com>
2021-06-06 11:11:32 +0900

    Time: 60 ms (76.61%), Space: 14.2 MB (81.67%) - LeetHub

--- sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
+++ sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -1,11 +1,7 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
         ans = 0
-        for num in nums:
-            ans += num
         for i in range(1, len(nums) + 1):
-            if i == 1:
-                continue
             for j in combinations(nums, i):
                 tmpVar = 0
                 for k in j:
