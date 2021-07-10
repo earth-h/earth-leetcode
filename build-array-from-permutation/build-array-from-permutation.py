f4a76f7ac0cc2bc38c79c97ba7c76da87465f12c
earth <70900028+earth-h@users.noreply.github.com>
2021-07-10 21:37:46 +0900

    Time: 168 ms (18.17%), Space: 14.5 MB (72.24%) - LeetHub

---
+++ build-array-from-permutation/build-array-from-permutation.py
@@ -0,0 +1,7 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = list()
+        for i in nums:
+            ans.append(nums[i])
+            
+        return ans
