d0f042f999b31502a206dd39bcef67c2ff4eef63
earth <70900028+earth-h@users.noreply.github.com>
2021-09-11 22:56:38 +0900

    Time: 64 ms (6.11%), Space: 14.3 MB (11.82%) - LeetHub

--- height-checker/height-checker.py
+++ height-checker/height-checker.py
@@ -1,7 +1,18 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
+        cnt = collections.Counter(heights)
+        i, ans = 1, 0
+        for h in heights:
+            while cnt[i] == 0:
+                i += 1
+            if i != h:
+                ans += 1
+            cnt[i] -= 1 # 현재 위치가 i여야 하므로 1 지움
+        return ans
+        '''
         ans = 0
         for i, j in list(zip(heights, sorted(heights))):
             if i != j:
                 ans += 1
         return ans
+        '''
