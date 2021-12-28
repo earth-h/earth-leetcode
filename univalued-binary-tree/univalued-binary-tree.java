dbb017d890a314c79af575aaaa8d8a178ec8559d
earth <70900028+earth-h@users.noreply.github.com>
2021-12-28 12:47:16 +0900

    Time: 0 ms (100.00%), Space: 36.3 MB (93.24%) - LeetHub

--- univalued-binary-tree/univalued-binary-tree.java
+++ univalued-binary-tree/univalued-binary-tree.java
@@ -14,21 +14,34 @@
  * }
  */
 class Solution {
-    boolean isUnival = true;
-    
     public boolean isUnivalTree(TreeNode root) {
-        dfs(root, root.val);
-        return isUnival;
+        if(root == null) return true;
+        
+        if(root.left != null && root.left.val != root.val)
+            return false;
+        if(root.right != null && root.right.val != root.val)
+            return false;
+        
+        return isUnivalTree(root.left) && isUnivalTree(root.right);
     }
-    
-    public void dfs(TreeNode node, int rootValue) {
-        if(node.val == rootValue && isUnival) {
-            if(node.left != null)
-                dfs(node.left, rootValue);
-            if(node.right != null) 
-                dfs(node.right, rootValue);
-        }
-        else
-            isUnival = false;
-    }
+
 }
+// class Solution {
+//     boolean isUnival = true;
+    
+//     public boolean isUnivalTree(TreeNode root) {
+//         dfs(root, root.val);
+//         return isUnival;
+//     }
+    
+//     public void dfs(TreeNode node, int rootValue) {
+//         if(node.val == rootValue && isUnival) {
+//             if(node.left != null)
+//                 dfs(node.left, rootValue);
+//             if(node.right != null) 
+//                 dfs(node.right, rootValue);
+//         }
+//         else
+//             isUnival = false;
+//     }
+// }
