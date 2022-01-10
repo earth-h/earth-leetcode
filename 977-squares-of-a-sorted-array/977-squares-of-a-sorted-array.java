1ea3a06fcc95dce4f6dc93fe73ac42a57170d26d
earth <70900028+earth-h@users.noreply.github.com>
2022-01-10 17:12:43 +0900

    Time: 4 ms (18.62%), Space: 53.2 MB (16.25%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,37 +1,45 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        List<Integer> minus = new ArrayList<>();
-        List<Integer> plus = new ArrayList<>();
-        for(int i: nums) {
-            if(i < 0) minus.add(0, i * i);
-            else plus.add(i * i);
-        }
-        if(minus.size() == 0) {
-            for(int idx = 0; idx < nums.length; idx++)
-                nums[idx] *= nums[idx];
-            return nums;
-        } else {
-            int[] result = new int[nums.length];
-            int idx = 0, i = 0, j = 0;
-            for(; i < minus.size() && j < plus.size();) {
-                if(minus.get(i) < plus.get(j))  {
-                    result[idx++] = minus.get(i);
-                    i++;
-                }
-                else {
-                    result[idx++] = plus.get(j);
-                    j++;
-                }
-            }
-            if(i < minus.size()) {
-                for(; i < minus.size(); i++)
-                    result[idx++] = minus.get(i);
-            }
-            else if(j < plus.size()) {
-                for(; j < plus.size(); j++)
-                    result[idx++] = plus.get(j);
-            }
-            return result;
-        }
+        for(int idx = 0; idx < nums.length; idx++)
+            nums[idx] *= nums[idx];
+        Arrays.sort(nums);
+        return nums;
     }
 }
+// class Solution {
+//     public int[] sortedSquares(int[] nums) {
+//         List<Integer> minus = new ArrayList<>();
+//         List<Integer> plus = new ArrayList<>();
+//         for(int i: nums) {
+//             if(i < 0) minus.add(0, i * i);
+//             else plus.add(i * i);
+//         }
+//         if(minus.size() == 0) {
+//             for(int idx = 0; idx < nums.length; idx++)
+//                 nums[idx] *= nums[idx];
+//             return nums;
+//         } else {
+//             int[] result = new int[nums.length];
+//             int idx = 0, i = 0, j = 0;
+//             for(; i < minus.size() && j < plus.size();) {
+//                 if(minus.get(i) < plus.get(j))  {
+//                     result[idx++] = minus.get(i);
+//                     i++;
+//                 }
+//                 else {
+//                     result[idx++] = plus.get(j);
+//                     j++;
+//                 }
+//             }
+//             if(i < minus.size()) {
+//                 for(; i < minus.size(); i++)
+//                     result[idx++] = minus.get(i);
+//             }
+//             else if(j < plus.size()) {
+//                 for(; j < plus.size(); j++)
+//                     result[idx++] = plus.get(j);
+//             }
+//             return result;
+//         }
+//     }
+// }
