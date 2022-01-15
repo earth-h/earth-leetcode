8f88d2a6f8536c199e54a92fb0bf7500dd54bcdc
earth <70900028+earth-h@users.noreply.github.com>
2022-01-15 21:34:48 +0900

    Time: 1 ms (61.69%), Space: 41.4 MB (31.44%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -4,18 +4,30 @@ class Solution {
         * target - numbers[i] > numbers[i] -> GO
         * target - numbers[i] <= numbers[i] -> Continue
         */
-        for(int i = 0; i < numbers.length; i++) {
-            int cnt = target - numbers[i];
-            if(cnt >= numbers[i]) {
-                for(int j = i + 1; j < numbers.length; j++) {
-                    if(cnt < numbers[j])
-                        break;
-                    else if(cnt == numbers[j])
-                        return new int[] {i + 1, j + 1};
-                }
-            } else continue;
+//         for(int i = 0; i < numbers.length; i++) {
+//             int cnt = target - numbers[i];
+//             if(cnt >= numbers[i]) {
+//                 for(int j = i + 1; j < numbers.length; j++) {
+//                     if(cnt < numbers[j])
+//                         break;
+//                     else if(cnt == numbers[j])
+//                         return new int[] {i + 1, j + 1};
+//                 }
+//             } else continue;
 
+//         }
+//         return new int[2];
+        
+        int left = 0, right = numbers.length - 1;
+        
+        while(numbers[left] + numbers[right] != target) {
+            int total = numbers[left] + numbers[right];
+            if(total > target) {
+                right--;
+            } else if(total < target) {
+                left++;
+            } else break;
         }
-        return new int[2];
+        return new int[] {left + 1, right + 1};
     }
 }
