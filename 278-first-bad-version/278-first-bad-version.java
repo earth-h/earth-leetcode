b4af61a3991e6b5488edb79ab2fe656f063a5c2e
earth <70900028+earth-h@users.noreply.github.com>
2022-01-16 15:14:33 +0900

    Time: 18 ms (36.69%), Space: 37.6 MB (30.30%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -3,15 +3,15 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        return firstBadVersion(1, n);
+        //return firstBadVersion(1, n);
         
-        // int start = 1, end = n;
-        // while(start < end) {
-        //     int mid = (start + end) / 2;
-        //     if(isBadVersion(mid) == false) start = mid + 1;
-        //     else end = mid - 1;
-        // }
-        // return start;
+        int start = 1, end = n;
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            if(isBadVersion(mid) == false) start = mid + 1;
+            else end = mid;
+        }
+        return start;
     }
     public int firstBadVersion(int start, int end) {
         int mid = start + (end - start) / 2;
