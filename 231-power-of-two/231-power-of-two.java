ed4c7fd95b77833e07fc2104baaa44929b3120e0
earth <70900028+earth-h@users.noreply.github.com>
2022-01-22 11:37:01 +0900

    Time: 1 ms (99.98%), Space: 36 MB (68.25%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,15 +1,20 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n == 0) return false;
-        else if(n == 1) return true;
-        
-        while(n != 2) {
-            if(n % 2 == 0) {
-                n /= 2;
-            }
-            else 
-                return false;
-        }
-        return true;
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
+// class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         if(n == 0) return false;
+//         else if(n == 1) return true;
+        
+//         while(n != 2) {
+//             if(n % 2 == 0) {
+//                 n /= 2;
+//             }
+//             else 
+//                 return false;
+//         }
+//         return true;
+//     }
+// }
