5bbd755ce399cff88ce0644788c6e8dcd544ca4f
earth <70900028+earth-h@users.noreply.github.com>
2022-01-30 11:45:59 +0900

    Time: 1 ms (62.39%), Space: 41.9 MB (6.05%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -1,15 +1,36 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
-        String charWord = Character.toString(ch);
-        String[] wordArray = word.split(charWord, 2);
-        
-        if(wordArray.length == 1 && word.substring(word.length() - 1) != charWord)
-            return word;
-        
-        StringBuffer sb = new StringBuffer(wordArray[0].concat(charWord));
-        
-        if(wordArray.length == 2)
-            return sb.reverse().toString() + wordArray[1];
-        return sb.reverse().toString();
+        char[] chArray = word.toCharArray();
+        int idx = -1;
+        for(int i = 0; i < chArray.length; i++) {
+            if(ch == chArray[i]) {
+                idx = i;
+                break;
+            }
+        }
+        if(idx == -1) return word;
+        else {
+            for(int i = 0; i <= idx / 2; i++) {
+                char tmp = chArray[i];
+                chArray[i] = chArray[idx - i];
+                chArray[idx - i] = tmp;
+            }
+        }
+        return String.valueOf(chArray);
     }
 }
+// class Solution {
+//     public String reversePrefix(String word, char ch) {
+//         String charWord = Character.toString(ch);
+//         String[] wordArray = word.split(charWord, 2);
+        
+//         if(wordArray.length == 1 && word.substring(word.length() - 1) != charWord)
+//             return word;
+        
+//         StringBuffer sb = new StringBuffer(wordArray[0].concat(charWord));
+        
+//         if(wordArray.length == 2)
+//             return sb.reverse().toString() + wordArray[1];
+//         return sb.reverse().toString();
+//     }
+// }
