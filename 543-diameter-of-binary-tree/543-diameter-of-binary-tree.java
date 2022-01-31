1c20cbeed42ec797b7c671e20fc3a66c49b813fb
earth <70900028+earth-h@users.noreply.github.com>
2022-01-31 16:02:10 +0900

    Time: 2 ms (15.00%), Space: 43.8 MB (5.14%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -16,52 +16,73 @@
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
         
-        int max = checkDiameterOfBinaryTree(root);
-        
-        Stack<TreeNode> stack = new Stack<>();
-        if(root.left != null) stack.push(root.left);
-        if(root.right != null) stack.push(root.right);
-        
-        while(!stack.empty()) {
-            TreeNode node = stack.pop();
-            if(node.left == null || node.right == null) continue;
-            stack.add(node.left);
-            stack.add(node.right);
-            int tmp = checkDiameterOfBinaryTree(node);
-            max = tmp > max ? tmp : max;
-        }
-        
-        return max;
+        return checkMaxDepth(root)[0];
     }
     
-    public int checkDiameterOfBinaryTree(TreeNode root) {
-        Queue<TreeNode> left = new LinkedList<>();
-        Queue<TreeNode> right = new LinkedList<>();
+    // bottom-up
+    public int[] checkMaxDepth(TreeNode root) {
+        if(root == null) return new int[] {0, 0};
         
-        if(root.left != null) left.add(root.left);
-        if(root.right != null) right.add(root.right);
+        int[] ans = new int[2]; // [best, height]
         
-        int leftDepth = 0;
-        int rightDepth = 0;
+        int[] left = checkMaxDepth(root.left);
+        int[] right = checkMaxDepth(root.right);
         
-        while(!left.isEmpty()) {
-            int len = left.size();
-            leftDepth++;
-            for(int i = 0; i < len; i++) {
-                TreeNode node = left.poll();
-                if(node.left != null) left.add(node.left);
-                if(node.right != null) left.add(node.right);
-            }
-        }
-        while(!right.isEmpty()) {
-            int len = right.size();
-            rightDepth++;
-            for(int i = 0; i < len; i++) {
-                TreeNode node = right.poll();
-                if(node.left != null) right.add(node.left);
-                if(node.right != null) right.add(node.right);
-            }
-        }
-        return leftDepth + rightDepth;
+        ans[0] = Math.max(left[1] + right[1], Math.max(left[0], right[0]));
+        ans[1] = 1 + Math.max(left[1], right[1]);
+        
+        return ans;
     }
 }
+// class Solution {
+//     public int diameterOfBinaryTree(TreeNode root) {
+        
+//         int max = checkDiameterOfBinaryTree(root);
+        
+//         Stack<TreeNode> stack = new Stack<>();
+//         if(root.left != null) stack.push(root.left);
+//         if(root.right != null) stack.push(root.right);
+        
+//         while(!stack.empty()) {
+//             TreeNode node = stack.pop();
+//             if(node.left == null || node.right == null) continue;
+//             stack.add(node.left);
+//             stack.add(node.right);
+//             int tmp = checkDiameterOfBinaryTree(node);
+//             max = tmp > max ? tmp : max;
+//         }
+        
+//         return max;
+//     }
+    
+//     public int checkDiameterOfBinaryTree(TreeNode root) {
+//         Queue<TreeNode> left = new LinkedList<>();
+//         Queue<TreeNode> right = new LinkedList<>();
+        
+//         if(root.left != null) left.add(root.left);
+//         if(root.right != null) right.add(root.right);
+        
+//         int leftDepth = 0;
+//         int rightDepth = 0;
+        
+//         while(!left.isEmpty()) {
+//             int len = left.size();
+//             leftDepth++;
+//             for(int i = 0; i < len; i++) {
+//                 TreeNode node = left.poll();
+//                 if(node.left != null) left.add(node.left);
+//                 if(node.right != null) left.add(node.right);
+//             }
+//         }
+//         while(!right.isEmpty()) {
+//             int len = right.size();
+//             rightDepth++;
+//             for(int i = 0; i < len; i++) {
+//                 TreeNode node = right.poll();
+//                 if(node.left != null) right.add(node.left);
+//                 if(node.right != null) right.add(node.right);
+//             }
+//         }
+//         return leftDepth + rightDepth;
+//     }
+// }
