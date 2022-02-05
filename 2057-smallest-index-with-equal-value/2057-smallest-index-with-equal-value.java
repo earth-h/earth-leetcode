f57e157b2f6d937230d4dd7854e5e1cae9b9be02
earth <70900028+earth-h@users.noreply.github.com>
2022-02-05 11:40:20 +0900

    Time: 1 ms (73.37%), Space: 46.3 MB (6.73%) - LeetHub

--- 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.java
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.java
@@ -1,7 +1,8 @@
 class Solution {
     public int smallestEqual(int[] nums) {
         for(int i = 0; i < nums.length; i++) {
-            if(i % 10 == nums[i]) return i;
+            if(i < 10 && i == nums[i]) return i;
+            else if(i % 10 == nums[i]) return i;
         }  
         return -1;
     }
