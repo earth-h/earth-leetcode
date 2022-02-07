d08a485cb8049799d72e7255699f0d5ece3cf875
earth <70900028+earth-h@users.noreply.github.com>
2022-02-07 22:37:57 +0900

    Time: 1 ms (76.45%), Space: 42.4 MB (5.20%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
@@ -1,13 +1,22 @@
 class Solution {
     public boolean checkString(String s) {
-        int b = 0;
-        
-        for(String a: s.split("")) {
-            if(b == 1 && a.compareTo("a") == 0) return false;
-            if(a.compareTo("b") == 0) {
-                b = 1;
-            }
+        for(int idx = 1; idx < s.length(); idx++) {
+            if(s.charAt(idx - 1) == 'b' && s.charAt(idx) == 'a') 
+                return false;
         }
         return true;
     }
 }
+// class Solution {
+//     public boolean checkString(String s) {
+//         int b = 0;
+        
+//         for(String a: s.split("")) {
+//             if(b == 1 && a.compareTo("a") == 0) return false;
+//             if(a.compareTo("b") == 0) {
+//                 b = 1;
+//             }
+//         }
+//         return true;
+//     }
+// }
