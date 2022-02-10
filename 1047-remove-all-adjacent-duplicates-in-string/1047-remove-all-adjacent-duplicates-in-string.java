07d219f9f03e8367d9655dd3c6e7642d5d3f205c
earth <70900028+earth-h@users.noreply.github.com>
2022-02-10 22:41:23 +0900

    Time: 188 ms (9.80%), Space: 75.2 MB (8.58%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,26 +1,42 @@
+// class Solution {
+//     public String removeDuplicates(String s) {
+//         String[] stringArray = s.split("");
+//         List<String> strList = new ArrayList<>();
+//         int idx = 0;
+//         for(idx = 0; idx < stringArray.length - 1; idx++) {
+//             if(stringArray[idx].equals(stringArray[idx + 1])) {
+//                 idx++;
+//                 continue;
+//             }
+//             if(strList.size() == 0 || !(strList.get(strList.size() - 1).equals(stringArray[idx]))) {
+//                 strList.add(stringArray[idx]);
+//             } else {
+//                 strList.remove(strList.size() - 1);
+//             }
+//         }
+//         if(idx < stringArray.length) {
+//             if(strList.size() == 0 || !(strList.get(strList.size() - 1).equals(stringArray[idx]))) {
+//                 strList.add(stringArray[idx]);
+//             } else {
+//                 strList.remove(strList.size() - 1);
+//             }
+//         }
+//         return String.join("", strList);
+//     }
+// }
 class Solution {
     public String removeDuplicates(String s) {
         String[] stringArray = s.split("");
         List<String> strList = new ArrayList<>();
-        int idx = 0;
-        for(idx = 0; idx < stringArray.length - 1; idx++) {
-            if(stringArray[idx].equals(stringArray[idx + 1])) {
-                idx++;
-                continue;
-            }
-            if(strList.size() == 0 || !(strList.get(strList.size() - 1).equals(stringArray[idx]))) {
-                strList.add(stringArray[idx]);
-            } else {
-                strList.remove(strList.size() - 1);
-            }
+        
+        for(String a : s.split("")) {
+            int size = strList.size();
+            if(size > 0 && strList.get(size - 1).equals(a))
+                strList.remove(size - 1);
+            else
+                strList.add(a);
         }
-        if(idx < stringArray.length) {
-            if(strList.size() == 0 || !(strList.get(strList.size() - 1).equals(stringArray[idx]))) {
-                strList.add(stringArray[idx]);
-            } else {
-                strList.remove(strList.size() - 1);
-            }
-        }
+        
         return String.join("", strList);
     }
 }
