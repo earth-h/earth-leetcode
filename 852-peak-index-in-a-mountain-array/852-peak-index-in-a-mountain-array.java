05cfd035ef73ced59682a8c839eb06cbe2130a17
earth <70900028+earth-h@users.noreply.github.com>
2022-02-12 17:13:28 +0900

    Time: 0 ms (100.00%), Space: 46.3 MB (12.57%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -1,10 +1,24 @@
+// class Solution {
+//     public int peakIndexInMountainArray(int[] arr) {
+//         // 오름차순 + 내림차순
+//         for(int i = 1; i < arr.length - 1; i++) {
+//             if(arr[i] > arr[i + 1])
+//                 return i;
+//         }
+//         return 0;
+//     }
+// }
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         // 오름차순 + 내림차순
-        for(int i = 1; i < arr.length - 1; i++) {
-            if(arr[i] > arr[i + 1])
-                return i;
+        int left = 0, right = arr.length - 1, middle;
+        while(left < right) {
+            middle = (left + right) / 2;
+            if(arr[middle] > arr[middle + 1])
+                right = middle;
+            else
+                left = middle + 1;
         }
-        return 0;
+        return left;
     }
 }
