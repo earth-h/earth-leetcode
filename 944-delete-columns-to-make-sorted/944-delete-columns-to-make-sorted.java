9ef01e6a16162bdc8a4443f3f3191ee1ca404040
earth <70900028+earth-h@users.noreply.github.com>
2022-02-16 23:48:57 +0900

    Time: 15 ms (39.37%), Space: 47.5 MB (26.24%) - LeetHub

--- 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.java
+++ 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.java
@@ -1,21 +1,36 @@
+// class Solution {
+//     public int minDeletionSize(String[] strs) {
+//         List<char[]> charStr = new ArrayList<>();
+        
+//         int len = strs[0].length(), idx = 0, result = 0;
+        
+//         for(String str: strs) {
+//             charStr.add(str.toCharArray());
+//         }
+        
+//         for(int i = 0; i < len; i++) {
+//             for(int j = 0; j < charStr.size() - 1; j++) {
+//                 if(charStr.get(j)[i] > charStr.get(j+1)[i]) {
+//                     result++;
+//                     break;
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// }
 class Solution {
     public int minDeletionSize(String[] strs) {
-        List<char[]> charStr = new ArrayList<>();
-        
-        int len = strs[0].length(), idx = 0, result = 0;
-        
-        for(String str: strs) {
-            charStr.add(str.toCharArray());
-        }
-        
-        for(int i = 0; i < len; i++) {
-            for(int j = 0; j < charStr.size() - 1; j++) {
-                if(charStr.get(j)[i] > charStr.get(j+1)[i]) {
+        int result = 0;
+
+        for(int i = 0; i < strs[0].length(); i++) 
+            for(int j = 0; j < strs.length - 1; j++)
+                if(strs[j].charAt(i) > strs[j+1].charAt(i)) {
                     result++;
                     break;
                 }
-            }
-        }
+        
+        
         return result;
     }
 }
