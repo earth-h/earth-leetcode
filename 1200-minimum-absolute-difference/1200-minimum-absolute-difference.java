c8b862e7fc82cae037830bc966cb84259d70dcc1
earth <70900028+earth-h@users.noreply.github.com>
2022-02-20 16:19:57 +0900

    Time: 22 ms (52.30%), Space: 68.7 MB (5.06%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -1,3 +1,26 @@
+// class Solution {
+//     public List<List<Integer>> minimumAbsDifference(int[] arr) {
+//         int minDis = 2000002;
+//         List<List<Integer>> result = new ArrayList<>();
+
+//         Arrays.sort(arr);
+        
+//         for(int i = 0; i < arr.length - 1; i++) {
+//             if(arr[i + 1] - arr[i] < minDis) {
+//                 result = new ArrayList<>();
+//                 List<Integer> curPair = new ArrayList<>(Arrays.asList(arr[i], arr[i + 1]));
+//                 result.add(curPair);
+//                 minDis = arr[i + 1] - arr[i];
+//             } else if(arr[i + 1] - arr[i] == minDis) {
+//                 List<Integer> curPair = new ArrayList<>(Arrays.asList(arr[i], arr[i + 1]));
+//                 result.add(curPair);
+//             }
+                
+//         }
+        
+//         return result;
+//     }
+// }
 class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
         int minDis = 2000002;
@@ -7,13 +30,11 @@ class Solution {
         
         for(int i = 0; i < arr.length - 1; i++) {
             if(arr[i + 1] - arr[i] < minDis) {
-                result = new ArrayList<>();
-                List<Integer> curPair = new ArrayList<>(Arrays.asList(arr[i], arr[i + 1]));
-                result.add(curPair);
+                result.clear();
+                result.add(new ArrayList<>(Arrays.asList(arr[i], arr[i + 1])));
                 minDis = arr[i + 1] - arr[i];
             } else if(arr[i + 1] - arr[i] == minDis) {
-                List<Integer> curPair = new ArrayList<>(Arrays.asList(arr[i], arr[i + 1]));
-                result.add(curPair);
+                result.add(new ArrayList<>(Arrays.asList(arr[i], arr[i + 1])));
             }
                 
         }
