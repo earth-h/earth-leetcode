b2ce2986643827576e0af81ab482b5eb0518013f
earth <70900028+earth-h@users.noreply.github.com>
2022-03-03 22:40:04 +0900

    Time: 23 ms (58.05%), Space: 40.7 MB (51.54%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.java
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -2,10 +2,11 @@ class Solution {
     public int countTriples(int n) {
         int ans = 0;
         for(int i = 1; i < n; i++) {
-            for(int j = 1; j < n; j++) {
+            for(int j = i;j < n; j++) {
                 double res = Math.sqrt(i * i + j * j);
                 if(res == Math.floor(res) && res <= n)
-                    ans++;
+                    if(i == j) ans++;
+                    else ans += 2;
             }
         }
         return ans;
