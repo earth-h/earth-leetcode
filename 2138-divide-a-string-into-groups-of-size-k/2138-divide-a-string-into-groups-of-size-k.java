d99aed37052654c318fa3ac8dba11b18a118301f
earth <70900028+earth-h@users.noreply.github.com>
2022-03-06 12:46:48 +0900

    Time: 8 ms (39.73%), Space: 43.6 MB (5.52%) - LeetHub

--- 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.java
+++ 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.java
@@ -4,14 +4,11 @@ class Solution {
         String[] result = new String[size];
         
         for(int idx = 0; idx < size; idx++) {
-            result[idx] = "";
             if(idx != size - 1) {
-                for(int i = 0; i < k; i++) 
-                    result[idx] += s.charAt(idx * k + i);
+                result[idx] = s.substring(idx * k, idx * k + k);
             }
             else {
-                for(int i = 0; i < k - (idx * k + k - s.length()); i++) 
-                    result[idx] += s.charAt(idx * k + i);
+                result[idx] = s.substring(idx * k, idx * k + k - (idx * k + k - s.length()));
                 for(int i = 0; i < idx * k + k - s.length(); i++)
                     result[idx] += fill;
             }
