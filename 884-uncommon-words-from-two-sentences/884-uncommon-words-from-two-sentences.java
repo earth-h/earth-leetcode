4537e8f6ff54d2e0da79ecbf432ab69ea368f2de
earth <70900028+earth-h@users.noreply.github.com>
2022-03-10 20:26:57 +0900

    Time: 4 ms (53.44%), Space: 43 MB (18.74%) - LeetHub

---
+++ 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.java
@@ -0,0 +1,40 @@
+import java.util.Map.Entry;
+
+class Solution {
+    public String[] uncommonFromSentences(String s1, String s2) {
+        Map<String, Integer> map = new HashMap<>();
+        int cnt = 0;
+        for(String s: s1.split(" ")) {
+            if(map.containsKey(s))
+                map.put(s, map.get(s) + 1);
+            else
+                map.put(s, 1);
+        }
+        for(String s: s2.split(" ")) {
+            if(map.containsKey(s))
+                map.put(s, map.get(s) + 1);
+            else
+                map.put(s, 1);
+        }
+        
+        /*
+        * WARNING: The Security Manager is deprecated and will be removed in a future release
+        * java.util.ConcurrentModificationException
+        * https://javarevisited.blogspot.com/2017/08/how-to-remove-key-value-pair-from-map-iteration-java-example.html#axzz7N88KZPny
+        */
+        // for(String key: map.keySet()) {
+        //     System.out.println(key);
+        //     if(map.get(key) > 1) 
+        //         map.remove(key);
+        // }
+        
+        Iterator<Entry<String, Integer>> iter = map.entrySet().iterator();
+        while(iter.hasNext()) {
+            Entry<String, Integer> entry = iter.next();
+            if(entry.getValue() > 1)
+                iter.remove();
+        }
+        
+        return map.keySet().toArray(new String[map.size()]);
+    }
+}
