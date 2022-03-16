2b5eacba185dab9e477efb6cc24b9dac6e0e86a0
earth <70900028+earth-h@users.noreply.github.com>
2022-03-16 22:27:47 +0900

    Time: 1 ms (100.00%), Space: 42.6 MB (46.49%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.java
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -1,18 +1,32 @@
+// class Solution {
+//     public int maxPower(String s) {
+//         int result = 0, tempResult = 1;
+//         char c = s.charAt(0);
+//         if(s.length() == 1) return 1;
+//         for(int i = 1; i < s.length(); i++) {
+//             if(c == s.charAt(i)) {
+//                 tempResult++;
+//             } else {
+//                 result = result > tempResult ? result : tempResult;
+//                 tempResult = 1;
+//                 c = s.charAt(i);
+//             } 
+//         }
+//         result =  result > tempResult ? result : tempResult;
+//         return result;
+//     }
+// }
 class Solution {
     public int maxPower(String s) {
-        int result = 0, tempResult = 1;
-        char c = s.charAt(0);
+        int result = 1;
         if(s.length() == 1) return 1;
-        for(int i = 1; i < s.length(); i++) {
-            if(c == s.charAt(i)) {
-                tempResult++;
+        for(int i = 1, tempResult = 1; i < s.length(); i++) {
+            if(s.charAt(i-1) == s.charAt(i)) {
+                if(++tempResult > result) result = tempResult;
             } else {
-                result = result > tempResult ? result : tempResult;
                 tempResult = 1;
-                c = s.charAt(i);
             } 
         }
-        result =  result > tempResult ? result : tempResult;
         return result;
     }
 }
